eeb7b980c5b2d0d199380f748d8f9a5b03b25852
21now <168331277+21now@users.noreply.github.com>
2025-07-03 09:40:17 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
-// This is just to allow render to start the app
-const { exec } = require("child_process");
-
-exec("n8n", (err, stdout, stderr) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log(stdout);
-});
+// This file is just a placeholder to satisfy Render's deployment.
+// The actual app is started via "npm start" which runs the n8n CLI.
